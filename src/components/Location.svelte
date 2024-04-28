d749ab74d5e1aeedf351904018d5b7373b31f556
mig-29 <mig@bluewin.ch>
2024-04-28 12:14:04 +0200

    First Commit von Mig
    	new file:   .github/workflows/deploy.yml
    	new file:   .gitignore
    	new file:   .vscode/extensions.json
    	new file:   .vscode/launch.json
    	new file:   README.md
    	new file:   astro.config.mjs
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   public/favicon.svg
    	new file:   src/components/Card.astro
    	new file:   src/components/Location.svelte
    	new file:   src/env.d.ts
    	new file:   src/layouts/Layout.astro
    	new file:   src/pages/index.astro
    	new file:   svelte.config.js
    	new file:   tsconfig.json

---
+++ src/components/Location.svelte
@@ -0,0 +1,40 @@
+<script>
+    let location = {
+        floorId : "00"
+    }
+
+
+</script>
+
+<h1 class="text-gradient">Location Component (svelte)</h1>
+
+Stockwerk auswählen:
+
+<select bind:value={location.floorId}  name="location" id="location">
+    <option value={"00"}>Default</option>
+    <option value={"01"}>Obergeschoss 1</option>
+    <option value={"02"}>Obergeschoss 2</option>
+    <option value={"03"}>Obergeschoss 3</option>
+    <option value={"04"}>Obergeschoss 4</option>
+    <option value={"05"}>Obergeschoss 5</option>
+    <option value={"U1"}>Untergeschoss 1</option>
+    <option value={"U2"}>Untergeschoss 2</option>
+</select>
+<p >
+    Location_No: 8600-Dübendorf-Zürichstrasse 30-<span class="yellow">{location.floorId}</span>-B440
+</p>
+<style>
+
+
+    select {
+        margin-left: 20px;
+        font-size: 1rem;
+        padding: 10px;
+        min-width: 200px;
+        border-radius: 5px;
+    }
+
+  .yellow{
+    color: yellow;
+  }
+</style>
