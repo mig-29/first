34b1d1f28203c4f8a10bd369a4663198eb0c08ac
mig-29 <mig@bluewin.ch>
2024-04-28 12:47:26 +0200

    Change Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  site: "https://mig-29.github.io",
-  base: "/location"
+  site: "https://mig-29.github.io"
 });
